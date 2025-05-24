57d3e62ac4df040bb5c98d91d03d2a91ce2a4d8a
kminjoo9093 <kminjoo9093@naver.com>
2025-05-25 01:27:43 +0900

    page-nav 언더바 수정

--- js/main.js
+++ js/main.js
@@ -14,34 +14,42 @@ window.addEventListener("wheel", (e) => {
 // section01 chevron
 showChevron();
 
-function showChevron(){
-  const section01 = document.querySelector('#section01');
-  const chevronObserver = new IntersectionObserver(entries=>{
-    entries.forEach(e=>{
-      if(e.isIntersecting){
-        document.querySelector('.ani-chevron').style.display = 'block';
-      } else {
-        document.querySelector('.ani-chevron').style.display = 'none';
-      }
-    })
-  }, {
-    threshold: 0.7  
-  })
+function showChevron() {
+  const section01 = document.querySelector("#section01");
+  const chevronObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((e) => {
+        if (e.isIntersecting) {
+          document.querySelector(".ani-chevron").style.display = "block";
+        } else {
+          document.querySelector(".ani-chevron").style.display = "none";
+        }
+      });
+    },
+    {
+      threshold: 0.7,
+    }
+  );
 
   chevronObserver.observe(section01);
 }
 
-
 // page-nav 클릭 언더바이동
-$(".page-nav li").click(function () {
-  $(this).siblings().removeClass("active");
-  $(this).addClass("active");
-});
+const pageNavs = document.querySelectorAll(".page-nav li");
+pageNavs.forEach((nav, _, array)=>{
+  nav.addEventListener('click', (e)=>{
+    e.preventDefault();
+    array.forEach(el=>{
+      el.classList.remove('active');
+    })
+    nav.classList.add('active');
+  })
+})
 
 // section intersectionObserver
 const section02 = document.querySelector("#section02"),
-      section03 = document.querySelector("#section03"),
-      section06 = document.querySelector("#section06");
+  section03 = document.querySelector("#section03"),
+  section06 = document.querySelector("#section06");
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((e) => {
